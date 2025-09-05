6dcb4017b8222c26bf0a69c633fbe5462f5f15a5
BeamlakF <beamlakfekadu562@gmail.com>
2025-09-05 21:35:57 +0300

    back-end, Sept 5-models created

--- Beam_backend/core/models.py
+++ Beam_backend/core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+
+
+class Articles(models.Model):
+    title = models.TextField() 
+    content = models.TextField(max_length = 1000)
+    slug = models.SlugField()
+    published_at = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField()
+    image = models.ImageField(upload_to='articles/')
+
+    def __str__(self):
+        return self.title
+
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=150)
+    email = models.EmailField()  
+    text = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.email})"
+    
+
+class CV(models.Model):
+    file = models.FileField(upload_to="cv/")
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"CV uploaded on {self.uploaded_at}"
