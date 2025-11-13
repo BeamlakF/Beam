a375945c1d9c412360ab46805617af36fd5d373a
BeamlakF <beamlakfekadu562@gmail.com>
2025-11-13 15:46:25 +0300

    Decided to do the backend all over again

--- Beam_backend/core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Article, ContactMessage, CV, Project
-
-admin.site.register(Article)
-admin.site.register(ContactMessage)
-admin.site.register(CV)
-admin.site.register(Project)
