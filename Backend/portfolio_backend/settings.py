e2a0fec531466c83d0362a138a5b66ce3056fd54
BeamlakF <beamlakfekadu562@gmail.com>
2025-11-14 00:40:01 +0300

    there is this thing called Deployment

--- Backend/portfolio_backend/settings.py
+++ Backend/portfolio_backend/settings.py
@@ -113,7 +113,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'  
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -122,6 +123,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = os.getenv('MEDIA_URL', '/media/')
 MEDIA_ROOT = os.path.join(BASE_DIR, os.getenv('MEDIA_ROOT', 'media'))
+
 
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
